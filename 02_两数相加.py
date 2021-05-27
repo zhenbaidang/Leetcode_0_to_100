f17985c59c646d826614d5f5f815ba549312eda7
Zhenbai <zhenbaidang@gmail.com>
2021-05-28 01:46:10 +0800

    Update 02_两数相加.py

--- 02_两数相加.py
+++ 02_两数相加.py
@@ -104,9 +104,56 @@ class Solution:
                         cur = cur.next
                         cur.val = jinwei
                         return result
+# ===========================================上述版本的简洁版=======================================
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        x = l1.val
+        y = l2.val
+        jinwei = 0
+
+        result = ListNode()
+        cur = result
+
+        while (jinwei+x+y) or l1 or l2: # l1没走到none节点 或 l2没走到None节点 或他俩都没节点了，但是前俩节点加出一个进位 这三种情况，都需要再新建一个节点，追加到result这个链表的后面
+            cur.next = ListNode((jinwei+x+y)%10)
+            cur = cur.next
+            jinwei = (jinwei+x+y) // 10 # 求出新的进位
+            if l1: # l1没走到None，就让他继续走
+                l1 = l1.next
+            if l2: # l2没走到None，就让他继续走
+                l2 = l2.next
+            x = l1.val if l1 else 0 # 求出新的x：如果l1走到None节点了，那就让x=0，不然的话就让x=l1的值
+            y = l2.val if l2 else 0 # 求出新的y 如果l2走到None节点了，那就让y=0 （但是注意，并非通过y=0就能推断l2已经没有后续的节点从而放弃添加新节点，只要l2还没走到头，就应该让循环继续）
+        return result.next
 
     
-    
-    
-    
+# ===========================================列表逆序=======================================
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# 建立一个节点值为0-9的链表
+head = ListNode(0)
+cur = head
+for i in range(1, 10):
+    cur.next = ListNode(i)
+    cur = cur.next
+# 开始逆序
+next = head.next # 保存head的next节点，防止丢掉head链表
+pre = None # 新的逆序链表的头节点
+while head: # 当原链表head没走到None时
+    next = head.next # 把头的next用next存一下
+    head.next = pre # 把现在的头的下一个指向逆序链表的头（把现有的逆序链表挂在现在这个头的后面）
+    pre = head # 把逆序链表的头指针移到当前的真正的头节点
+    head = next # 把正序链表的头挪到上面保存好的next上
+# 逆序链表输出一波
+while pre:
+    print(pre.val)
+    pre = pre.next
+
       
