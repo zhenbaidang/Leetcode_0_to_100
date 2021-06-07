c8a944ee75e229f5377d3f37690d734e623cb6c3
Zhenbai <zhenbaidang@gmail.com>
2021-06-08 00:57:53 +0800

    Update 04_寻找两个正序数组的中位数.py

--- 04_寻找两个正序数组的中位数.py
+++ 04_寻找两个正序数组的中位数.py
@@ -60,3 +60,32 @@ class Solution:
             return (result[-1]+result[-2])/2
         else:
             return result[-1]
+
+# 事实上不需要把整个合并数组都找到，找到中间的一个值或两个值即可
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        a_len = len(nums1)
+        b_len = len(nums2)
+        sum_len = a_len + b_len
+        a_cur = b_cur = 0 # 两个游标负责取nums1和nums2的值
+        left = right = -1 # 定义两个槽位用来保存两个要做平均的中位数或者用right来保存中位数，left弃之不用
+        if sum_len == 0:
+            return 0
+        for i in range(sum_len//2+1): # 不管合并数组最终是奇数还是偶数，我都要计算到下标为sum_len//2这个值（这个值是要保留的），故range(sum_len//2+1)
+            left = right # 把右边的值赋给左边保存，下面对right进行更新
+            # 什么时候新的值是用nums1中取得值？1. nums1还没取到末尾（a_cur < a_len） 2. 同时nums2取完了（b_cur >= b_len） 3. 又或者nums2没取完，但是nums1在a_cur游标处的值比nums2在b_cur处的值小
+            if (a_cur < a_len) and ((b_cur >= b_len) or (nums1[a_cur] < nums2[b_cur])): 
+                right = nums1[a_cur]
+                a_cur += 1
+            else:
+                right = nums2[b_cur]
+                b_cur += 1
+        # 循环最后一次执行时，应该是要处理合并数组的下标为sum_len//2处的值，即此时right就是sum_len//2的对应值，而left 是他前一个值sum_len//2-1
+        
+        # 判断一下奇数还是偶数，决定返回right不用left还是返回right和left的均值
+        if sum_len % 2:
+            return right
+        else:
+            return (left+right)/2
+
+        
